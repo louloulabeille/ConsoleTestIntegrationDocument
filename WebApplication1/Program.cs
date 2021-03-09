99aaba3320948102fb57c02adfd7961dccfc9f4b
louloulabeille <louloulabeille@hotmail.com>
2021-03-09 17:11:13 +0100

    testMerge ca va partir en couille

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -15,7 +15,11 @@ namespace WebApplication1
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+        /// <summary>
+        /// create host
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
