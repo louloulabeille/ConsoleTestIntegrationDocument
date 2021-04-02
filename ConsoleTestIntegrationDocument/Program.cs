538647d6f5e65d0e70a53edafd7a545bf344599a
louloulabeille <louloulabeille@hotmail.com>
2021-04-02 09:26:33 +0200

    modif

--- ConsoleTestIntegrationDocument/Program.cs
+++ ConsoleTestIntegrationDocument/Program.cs
@@ -8,6 +8,7 @@ using OpenXmlHelpers.Word;
 using DefautAfpaBriveContext;
 using AfpaBrive;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace ConsoleTestIntegrationDocument
 {
@@ -17,7 +18,7 @@ namespace ConsoleTestIntegrationDocument
         static void Main(string[] args)
         {
             string path = Directory.GetCurrentDirectory() + @"\Document\TestConvention.docx";
-            if (File.Exists(path))
+            if (!File.Exists(path))
             {
                 string pathCopy = Directory.GetCurrentDirectory() + @"\Document\Copy.docx";
                 File.Copy(path, pathCopy);
@@ -66,11 +67,30 @@ namespace ConsoleTestIntegrationDocument
                             item.ReplaceWithText("");
                         }
                     }
-                }
-                    
+                }       
             }
+
+            string toto = HashPassword("toto");
         }
 
+        public static string HashPassword(string password)
+        {
+            byte[] salt;
+            byte[] buffer2;
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
+            {
+                salt = bytes.Salt;
+                buffer2 = bytes.GetBytes(0x20);
+            }
+            byte[] dst = new byte[0x31];
+            Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
+            Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
+            return Convert.ToBase64String(dst);
+        }
 
     }
 }
